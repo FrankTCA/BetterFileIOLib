e3eaa19b9527a1097b6c01f5989cf7318e879e9d
FrankTCA <frank@steamedveggie.com>
2017-04-04 18:11:24 -0500

    File.java update dev0.0.1

--- src/BetterFileIO/FileManagement/File.java
+++ src/BetterFileIO/FileManagement/File.java
@@ -1,1 +1,12 @@
 package BetterFileIO.FileManagement;
+
+public class File {
+  FilePath path = null;
+  public File(FilePath path1) {
+    path = path1;
+  }
+  
+  public boolean create() {
+    
+  }
+}
