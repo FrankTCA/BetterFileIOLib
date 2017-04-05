0186270ed9fc5ae0782f5fded8e21b80c50b8d77
FrankTCA <frank@steamedveggie.com>
2017-04-05 06:51:41 -0500

    FilePath.java update dev0.1.0
    Fixed small errors and added comments

--- src/BetterFileIO/FileManagement/FilePath.java
+++ src/BetterFileIO/FileManagement/FilePath.java
@@ -3,7 +3,11 @@ package BetterFileIO.FileManagement;
 import java.io.File;
 
 public class FilePath {
+    
+  // important variables
   String path = null;
+  
+  // class initializer
   public FilePath(String path1) {
     String os = System.getProperty("os.name");
     if (os.equalsIgnoreCase("windows")) {
@@ -18,25 +22,29 @@ public class FilePath {
     path = path1;
   }
   
-  public String getPath() {
+  // get the file path as a string
+  public String getPathAsString() {
     return path;
   }
   
+  // converts file into BetterFileIO.FileManagement.File
   public File convertPathToStandardFile() {
     File file = new File(path);
     return file;
   }
   
+  // Converts a unix based file path to a windows based file path because windows uses \ while unix-based operating systems use /.
   public String convertUnixOrUnixBasedOperatingSystemPathToWindows() {
     char[] pathCh = path.toCharArray();
     for (int i = pathCh.length; i > -1; i--) {
-      if (pathCh[i] == "/") {
+      if (pathCh[i] == '/') {
         pathCh[i] = '\\';
       }
     }
     return new String(pathCh);
   }
   
+  // Vise-versa
   public String convertWindowsPathToUnixOrUnixBasedOperatingSystemPath() {
     char[] pathCh = path.toCharArray();
     for (int i = pathCh.length; i > -1; i--) {
@@ -47,10 +55,12 @@ public class FilePath {
     return new String(pathCh);
   }
   
+  // rewrites the file path without change by operating system
   public void rewritePathWithoutUsingUniversalOperatingSystemConversion(String path1) {
     path = path1;
   }
   
+  // rewrites the path with change by operating system.
   public void rewritePathWithUsingUniversalOperatingSystemConversion(String path1) {
     String os = System.getProperty("os.name");
     if (os.equalsIgnoreCase("windows")) {
