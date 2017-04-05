10894dba1140d76b7b775a1fd8ed5546d0ba2adc
FrankTCA <frank@steamedveggie.com>
2017-04-05 14:42:44 -0500

    Directory.java update dev0.0.0
    Similar to File.java, but for directories.

--- src/BetterFileIO/FileManagement/Directory.java
+++ src/BetterFileIO/FileManagement/Directory.java
@@ -1,1 +1,54 @@
 package BetterFileIO.FileManagement;
+
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+public class Directory {
+  java.io.File directory;
+  FilePath path;
+  
+  public Directory(FilePath path1) {
+    path = path1;
+    directory = new File(path1.getPathAsString());
+  }
+  
+  public boolean create() throws IOException {
+    if (!directory.exists()) {
+      directory.mkdir();
+      return true;
+    }
+    return false;
+  }
+  
+  public File[] findAllChildren(int buffer) {
+    File[] children = new File[buffer];
+    int x = 0;
+    for (java.io.File f : directory.listFiles()) {
+      children[x] = new File(new FilePath(f.getAbsolutePath()));
+      x++;
+    }
+    return children;
+  }
+  
+  public boolean delete(int directoryBuffer) throws FileNotFoundException, IOException {
+    if (directory.exists()) {
+      File[] children = findAllChildren(directoryBuffer);
+      int index = 0;
+      while (children.getFilePath().getPathAsString() != null) {
+        children[index].delete();
+        index++;
+      }
+      directory.delete();
+      return true;
+    }
+    return false;
+  }
+  
+  public java.io.File getStandardLibraryFile() {
+    return directory;
+  }
+  
+  public FilePath getFilePath() {
+    return path;
+  }
+}
