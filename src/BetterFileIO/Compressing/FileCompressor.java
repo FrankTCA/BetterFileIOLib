03071a1d14e10e82d5635bcee079c42c8f60ad57
FrankTCA <frank@steamedveggie.com>
2017-04-05 12:56:11 -0500

    FileCompressor update dev0.0.0
    Not done yet

--- src/BetterFileIO/Compressing/FileCompressor.java
+++ src/BetterFileIO/Compressing/FileCompressor.java
@@ -1,1 +1,26 @@
 package BetterFileIO.Compressing;
+
+import BetterFileIO.FileManagement.File;
+import BetterFileIO.FileManagement.FilePath;
+
+public class FileCompressor {
+  
+  // important variables
+  File toFile;
+  File fromFile;
+  
+  // class initializer
+  public FileCompressor(File fromFile1, FilePath toFilePath) {
+    fromFile = fromFile1;
+    toFile = new File(toFilePath);
+  }
+  
+  // compress file to file
+  public void compressFile() throws FileNotFoundException, IOException {
+    java.io.File toFile1 = new java.io.File(fromFile.getFilePath().getPathAsString());
+    FileOutputStream fos = new FileOutputStream(fromFile.getFilePath().getPathAsString());
+    ZipOutputStream zos = new ZipOutputSteam(fos);
+    zos.putNextEntry(new ZipEntry(fromFile.getFilePath().getPathAsString()));
+    
+  }
+}
