fce15f5ce9c37217f1fd31569fad664f53a92fff
FrankTCA <frank@steamedveggie.com>
2017-04-05 14:54:17 -0500

    DirectoryCompressor update 0.0.0

--- src/BetterFileIO/Compressing/DirectoryCompressor.java
+++ src/BetterFileIO/Compressing/DirectoryCompressor.java
@@ -1,1 +1,30 @@
 package BetterFileIO.Compressing;
+
+import BetterFileIO.FileManagement.Directory;
+import BetterFileIO.FileManagement.File;
+import java.util.List;
+
+public class DirectoryCompressor {
+  
+  // important variables
+  Directory inputFile;
+  File outputFile;
+  
+  // class initializer
+  public DirectoryCompressor(Directory inputFile1, FilePath outputFilePath) {
+    inputFile = inputFile1;
+    outputFile = new File(outputFilePath);
+  }
+  
+  // compresser
+  public void compress() {
+    byte[] buffer = new byte[1024];
+    String source = inputFile.getFilePath().getPathAsString();
+    FileOutputStream fos = null;
+    ZipOutputStream zos = null;
+    
+    FileInputStream in = null;
+    
+    for (String file : )
+  }
+}
