19c35f5f09155c556418fae8ba95eaf40c0c4b9a
FrankTCA <frank@steamedveggie.com>
2017-04-05 12:36:26 -0500

    SimpleFileWriter.java update 0.2.0
    Created all methods as of now, and added comments on top of all methods.

--- src/BetterFileIO/FileManagement/readwrite/SimpleFileWriter.java
+++ src/BetterFileIO/FileManagement/readwrite/SimpleFileWriter.java
@@ -1,6 +1,10 @@
 package BetterFileIO.FileManagement.readwrite;
 
 import BetterFileIO.FileManagement.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 public class SimpleFileWriter {
   
@@ -13,17 +17,32 @@ public class SimpleFileWriter {
   }
   
   // writes entire file from one string. Will ovverride any other text already included in file.
-  public void writeFileFromString(String toWrite) {
+  public void writeFileFromString(String toWrite) throws FileNotFoundException, IOException {
     java.io.File theFile = new java.io.File(file.getFilePath().getPathAsString());
     BufferedWriter writer = new BufferedWriter(new FileWriter(theFile, false));
     writer.write(toWrite);
+    writer.newLine();
     writer.flush();
     writer.close();
   }
   
-  public void writeNewLineToFile(String line) {
+  // write a new line to the file
+  public void writeNewLineToFile(String line) throws FileNotFoundException, IOException {
     java.io.File theFile = new java.io.File(file.getFilePath().getPathAsString());
-    BufferedWriter writer = new BufferedWriter(new FileWriter(theFile));
-    
+    BufferedWriter writer = new BufferedWriter(new FileWriter(theFile, true));
+    writer.write(line);
+    writer.newLine();
+    writer.flush();
+    writer.close();
+  }
+  
+  // get the Java SDK BufferedWriter class
+  public BufferedWriter getBufferedWriter() {
+    return new BufferedWriter(new FileWriter(new java.io.File(file.getFilePath().getPathAsString())));
+  }
+  
+  // Get the BetterFileIO.FileManagement.File class within this class
+  public File getFile() {
+    return file;
   }
 }
