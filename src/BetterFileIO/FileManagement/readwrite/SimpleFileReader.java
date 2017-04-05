f3f1b14834bc14578970c5a3b9616e27428d05e9
FrankTCA <frank@steamedveggie.com>
2017-04-05 10:56:24 -0500

    SimpleFileReader.java update dev.0.1.0
    Added most methods, and added comments on top of all methods.

--- src/BetterFileIO/FileManagement/readwrite/SimpleFileReader.java
+++ src/BetterFileIO/FileManagement/readwrite/SimpleFileReader.java
@@ -8,12 +8,15 @@ import java.io.IOException;
 
 public class SimpleFileReader {
     
+    // important variables
     File file = null;
     
+    // class initializer
     public SimpleFileReader(File file1) {
         file = file1;
     }
     
+    // reads entire file and returns the text inside of the file in the form of a string
     public String convertFileToString() throws FileNotFoundException, IOException {
         java.io.File theFile = new java.io.File(file.getFilePath().getPathAsString());
         BufferedReader reader = new BufferedReader(new FileReader(theFile));
@@ -25,5 +28,37 @@ public class SimpleFileReader {
         return wholeFile;
     }
     
+    // reads entire file and returns the text inside as a String array, sperated by lines. (Requires buffer)
+    public String[] convertFileToArrayOfLines(int buffer) throws FileNotFoundException, IOException {
+        java.io.File theFile = new java.io.File(file.getFilePath().getPathAsString());
+        BufferedReader reader = new BufferedReader(new FileReader(theFile));
+        String line;
+        String[] wholeFile = new String[buffer];
+        int x = 0;
+        while ((line = reader.readLine()) != null) {
+            wholeFile[x] = line;
+            x++;
+        }
+        return wholeFile;
+    }
     
+    // gets a certain line in a file by the number of the line. Returns as a String.
+    public String getLineByNumber(int lineID) throws FileNotFoundException, IOException {
+        java.io.File theFile = new java.io.File(file.getFilePath().getPathAsString());
+        BufferedReader reader = new BufferedReader(new FileReader(theFile));
+        String line;
+        int x = 0;
+        while ((line = reader.readLine()) != null) {
+            if (x = lineID) {
+                return line;
+            }
+            x++;
+        }
+        return null;
+    }
+    
+    // returns the file that is being read.
+    public File getFile() {
+        return file;
+    }
 }
