1fe2ee3ad50bca4c975d914aed40f8fbec52e4bb
FrankTCA <frank@steamedveggie.com>
2017-04-05 06:53:00 -0500

    File.java update dev.0.1.0
    Added new methods.

--- src/BetterFileIO/FileManagement/File.java
+++ src/BetterFileIO/FileManagement/File.java
@@ -1,12 +1,75 @@
 package BetterFileIO.FileManagement;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class File {
   FilePath path = null;
+  java.io.File file = null;
   public File(FilePath path1) {
     path = path1;
+    file = new java.io.File(path.getPathAsString());
   }
   
   public boolean create() {
-    
+    if (file.exists()) {
+        try {
+            file.createNewFile();
+            return true;
+        } catch (IOException ex) {
+            return false;
+        }
+    }
+    return false;
+  }
+  
+  public boolean delete() {
+      if (file.exists()) {
+          file.delete();
+          return true;
+      }
+      return false;
+  }
+  
+  public boolean rename(FilePath to) {
+      java.io.File toFile = new java.io.File(to.getPathAsString());
+      try {
+          BufferedReader reader = new BufferedReader(new FileReader(file));
+          String line = "";
+          String fileText = "";
+          try {
+              while ((line = reader.readLine()) != null) {
+                  fileText = fileText + "\n" + line;
+              }
+          } catch (IOException ex) {
+              return false;
+          }
+          BufferedWriter writer = new BufferedWriter(new FileWriter(toFile));
+          toFile.createNewFile();
+          writer.write(fileText);
+          writer.flush();
+          writer.close();
+          path = to;
+          file = toFile;
+          return true;
+      } catch (FileNotFoundException ex) {
+          return false;
+      } catch (IOException ex) {
+          return false;
+      }
+  }
+  
+  public java.io.File getStandardLibraryFile() {
+      return file;
+  }
+  
+  public FilePath getFilePath() {
+      return path;
   }
 }
