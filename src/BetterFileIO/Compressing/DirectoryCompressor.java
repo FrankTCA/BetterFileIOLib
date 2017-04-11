4f3cefdc37c20d8aced157b971599807f47de60e
FrankTCA <frank@steamedveggie.com>
2017-04-11 18:01:53 -0500

    DirectoryCompressor.java update dev0.0.1
    Minor bug fixes

--- src/BetterFileIO/Compressing/DirectoryCompressor.java
+++ src/BetterFileIO/Compressing/DirectoryCompressor.java
@@ -1,6 +1,13 @@
 package BetterFileIO.Compressing;
 
 import BetterFileIO.FileManagement.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+import org.apache.commons.io.IOUtils;
 
 public class DirectoryCompressor {
   // important variables
@@ -14,17 +21,17 @@ public class DirectoryCompressor {
   }
   
   // compress zip file
-  public void compress() {
+  public void compress() throws FileNotFoundException, IOException {
     ZipOutputStream zipFile = new ZipOutputStream(new FileOutputStream(input.getStandardLibraryFile()));
     compressDirectoryToZipFile(output.getFilePath().getPathAsString(), input.getFilePath().getPathAsString(), zipFile);
     IOUtils.closeQuietly(zipFile);
   }
   
   // private method. Do not use
-  private static void compressDirectoryToZipFile(String rootDir, String sourceDir, ZipOutputStream out) {
+  private static void compressDirectoryToZipFile(String rootDir, String sourceDir, ZipOutputStream out) throws FileNotFoundException, IOException {
       for (java.io.File file : new java.io.File(sourceDir).listFiles()) {
       if (file.isDirectory()) {
-        compressDirectoryToZipFile(rootDir, sourceDir + java.io.File.seperator + file.getName(), out);
+        compressDirectoryToZipFile(rootDir, sourceDir + java.io.File.pathSeparator + file.getName(), out);
       } else {
         ZipEntry entry = new ZipEntry(sourceDir.replace(rootDir, "") + file.getName());
         out.putNextEntry(entry);
